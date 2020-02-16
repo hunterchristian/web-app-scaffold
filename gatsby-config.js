b4f9ecaeed61236b816e8af4259664ae69238d7c
Hunter Hodnett <hchodnett@gmail.com>
2020-02-16 09:37:34 -0600

    Replace gatsby-starter site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Web App Scaffold`,
+    description: `A low overhead and high performance web application scaffold`,
+    author: `@hunterhod`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `web-app-scaffold`,
+        short_name: `scaffold`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
